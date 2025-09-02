56ca90e3ace2f99b6eeb3abbe527bc1da2d71328
Anshul <anshulraturi007@gmail.com>
2025-09-02 12:28:30 +0530

    basic setup of kafka

---
+++ apps/server/src/services/kafka.ts
@@ -0,0 +1,14 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  brokers: [],
+});
+
+export async function createProducer() {
+  const producer = kafka.producer();
+  await producer.connect();
+  return producer;
+}
+
+
+export default kafka;
