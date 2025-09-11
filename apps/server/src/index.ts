399b6756caed44da0d2ad1e2e9e4102a256b8fe6
Anshul <anshulraturi007@gmail.com>
2025-09-12 00:22:00 +0530

    add backend health check route to keep server awake by requesting through cron job or uptime-robot

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,9 +5,18 @@ import SocketService from "./services/socket";
 async function init() {
   const socketService = new SocketService();
   // startMessageConsumer();
-
-  const httpServer = http.createServer();
   const PORT = parseInt(process.env.PORT || "8000", 10); // <-- Fix here
+
+  const httpServer = http.createServer((req, res) => {
+    ////Health check route
+    if (req.url === "/" && req.method === "GET") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Server is alive"); // ✅ Health check
+    } else {
+      res.writeHead(404);
+      res.end("Not Found");
+    }
+  });
   socketService.io.attach(httpServer);
 
   httpServer.listen(PORT, "0.0.0.0", () => {
